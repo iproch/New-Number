359a350478a68b55d357e52da3dd1b8d77f5f8af
Isaac Proch <isaac@iproch.design>
2019-12-03 23:14:24 +0000

    Adds Scanner for user input and prompts user to enter a number.

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner userInput = new Scanner(System.in);
+
+    System.out.println("Please enter a number:");
+    int userNum = userInput.nextInt();
+
+
   }
 }
