4ff5d4dcda80ee418eab07dffc5b2121c3539a84
Isaac Proch <isaac@iproch.design>
2019-12-04 23:53:15 +0000

    Adds working functionality for the increment. Issue present is that 9 increments to 1.

--- Main.java
+++ Main.java
@@ -6,7 +6,25 @@ class Main {
 
     System.out.println("Please enter a number:");
     int userNum = userInput.nextInt();
+    int y = 0;
+    int x;
 
+    while(userNum > 0){
+      x = userNum % 10;
+      x++;
+      y = y * 10 + x;
+      userNum = userNum / 10;
+    }
+    
+    userNum = y;
+    y = 0;
+    
+    while(userNum > 0){
+      x = userNum % 10;
+      y = y * 10 + x;
+      userNum = userNum / 10;
+    }
 
+    System.out.println("Result:"+y);
   }
 }
